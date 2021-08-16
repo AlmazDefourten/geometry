4f8d0ca25588f7f9ae6ab8e223df8a2d4a7308fd
almaz.sahabiev@outlook.com <almaz.sahabiev@outlook.com>
2021-08-16 23:04:52 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace geometry
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 405);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.textBox1.Location = new System.Drawing.Point(52, 453);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 534);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FunctionParsing.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace geometry
+{
+    class FunctionParsing
+    {
+        public string m_functionInput;
+        private int startIndex = 0;
+        private int endIndex = 0;
+        public FunctionParsing(string functionInput) { m_functionInput = functionInput; }
+
+        private static bool isNumber(char input)
+        {
+            if (input == '0' || input == '1' || input == '2' || input == '3' || input == '4' || input == '5'
+                || input == '6' || input == '7' || input == '8' || input == '9') { return true; }
+            else { return false; }
+        }
+        private int getPriority(char input)
+        {
+            if (input == '^') { return 0; }
+            else if (input == '(') { return 1; }
+            else if (input == '*' || input == '/' || input == '^') { return 2; }
+            else if (input == '+' || input == '-') { return 3; }
+            else if (isNumber(input)) { return 4; }
+            return 10;
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        private int getBestPriority(string input)
+        {
+            // уровни приоритета 1 - ( ) 2 - *, /, ^ 3 - +, -, 4 - numbers
+            int bestPriority = 9999;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int priority;
+                priority = getPriority(input[i]);
+                if (priority < bestPriority) { bestPriority = priority; }
+            }
+            return bestPriority;
+        }
+        
+        private bool isOperator(char input)
+        {
+            if (input == '*' || input == '/' || input == '-' || input == '+')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string getLeftOperand(string input, int indexOfOperand)
+        {
+            
+            indexOfOperand--;
+            string operand = "";
+            char perem = input[indexOfOperand];
+            while (isNumber(perem))
+            {
+                operand += perem;
+                startIndex = indexOfOperand;
+                indexOfOperand--;
+                perem = input[indexOfOperand];
+                
+            }
+            operand = ReverseString(operand);
+            return operand;
+        }
+
+        private string getRightOperand(string input, int indexOfOperand)
+        {
+            indexOfOperand ++;
+            char perem = input[indexOfOperand];
+            string operand = "";
+            while (isNumber(perem))
+            {
+                operand += perem;
+                endIndex = indexOfOperand;
+                indexOfOperand++;
+                perem = input[indexOfOperand];
+            }
+            return operand;
+        }
+
+        public int calculation(int bestPriority, string input)
+        {
+            // надо заменить скобки аргументов ф-й на [ и ] а так же поменять кодовыми буквами sin = s, cos = c и тд
+            input = input.Insert(0, "q");
+            input = input + "q";
+            while (bestPriority == 1)
+            {
+                int indexOfFirstBracket = input.LastIndexOf('(');
+                int indexOfSecondBracket = input.IndexOf(")");
+                string newString = input.Substring(indexOfFirstBracket + 1, indexOfSecondBracket - indexOfFirstBracket - 1);
+                int znachenie = calculation(getBestPriority(newString), newString);
+                input = input.Remove(indexOfFirstBracket, indexOfSecondBracket - indexOfFirstBracket + 1);
+                input = input.Insert(indexOfFirstBracket, znachenie.ToString());
+                Debug.WriteLine(input);
+                bestPriority = getBestPriority(input);
+            }
+            while(bestPriority == 2)
+            {
+                
+                char operat;
+                int inpLen = input.Length;
+                for (int i = 0; i < inpLen; i++)
+                {
+                    if(isOperator(input[i]))
+                    {
+                        operat = input[i];
+                        
+                        int leftOperand = Convert.ToInt32(getLeftOperand(input, i));
+                        int rightOperand = Convert.ToInt32(getRightOperand(input, i));
+                        if (operat == '*')
+                        {
+                            int result = leftOperand * rightOperand;
+                            input = input.Remove(startIndex, endIndex - startIndex + 1);
+                            input = input.Insert(startIndex, result.ToString());
+                        }
+                        else if (operat == '/') {
+                            int result;
+                                result = leftOperand / rightOperand;
+                                input = input.Remove(startIndex, endIndex - startIndex + 1);
+                                input = input.Insert(startIndex, result.ToString());
+                                
+                        }
+
+                        
+                        inpLen = input.Length;
+                    }
+                }
+                Debug.WriteLine(input);
+                bestPriority = getBestPriority(input);
+            }
+
+            while (bestPriority == 3)
+            {
+                Debug.WriteLine(input);
+                char operat;
+                int inpLen = input.Length;
+                for (int i = 0; i < inpLen; i++)
+                {
+                    if (isOperator(input[i]))
+                    {
+                        operat = input[i];
+
+                        int leftOperand = Convert.ToInt32(getLeftOperand(input, i));
+                        int rightOperand = Convert.ToInt32(getRightOperand(input, i));
+                        if (operat == '+')
+                        {
+                            int result = leftOperand + rightOperand;
+                            input = input.Remove(startIndex, endIndex - startIndex + 1);
+                            input = input.Insert(startIndex, result.ToString());
+                        }
+                        else if (operat == '-')
+                        {
+                            int result;
+                            result = leftOperand - rightOperand;
+                            input = input.Remove(startIndex, endIndex - startIndex + 1);
+                            input = input.Insert(startIndex, result.ToString());
+
+                        }
+
+                        i = startIndex - 1;
+                        inpLen = input.Length;
+                    }
+                }
+                bestPriority = getBestPriority(input);
+            }
+            if (bestPriority == 4)
+            {
+                input = input.Replace("q", "");
+                return Convert.ToInt32(input);
+            }
+            return 0;
+        }
+        public int getY(int x)
+        {
+            int bestPriority = getBestPriority(m_functionInput);
+            int y;
+            string perem = m_functionInput;
+            int result = calculation(bestPriority, perem);
+            return result;
+        }
+    }
+}
