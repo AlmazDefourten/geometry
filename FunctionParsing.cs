f75470809fcd3f31b3999202a7adfe7d1fa6a2a2
almaz.sahabiev@outlook.com <almaz.sahabiev@outlook.com>
2021-08-18 00:53:08 +0300

    теперь есть степени

--- FunctionParsing.cs
+++ FunctionParsing.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-//проверка коммита
+//проверка коммит
 namespace geometry
 {
     class FunctionParsing
